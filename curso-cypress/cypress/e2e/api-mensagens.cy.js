bcce41981515284033b05b49fe45dafcbdcf9376
Yoshinobu21 <enzoyoshinobu@gmail.com>
2025-07-10 16:10:48 -0300

    Aula 5

--- curso-cypress/cypress/e2e/api-mensagens.cy.js
+++ curso-cypress/cypress/e2e/api-mensagens.cy.js
@@ -1,20 +1,17 @@
 describe('Api Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcwOTA0MTMwNSwiZXhwIjoxNzA5MzAwNTA1fQ.nhiaUyKdvN8RVxCkvne2gyI5n_pYnY_OMMdogMMqOlc`
-
     //Atenção, o token possui um prazo de validade. Dessa forma, você deverá realizar o login e copiar o toker presente no Local Storage do seu navegador.
+    //Você encontrará um cypress.en.json_exemplo para entender como o token deve ser inserido no arquivo cypress.env.json
      
      it('Mensagens da API', () => {
             cy.request({
              method: 'GET' ,
              url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-             headers: { authorization }
+             headers: Cypress.env()
                    
                 }).then((res) => {
                     expect(res.status).to.be.equal(200)
                     expect(res.body).is.not.empty
-                    expect(res.body).to.have.property('msg')
-                                 
+                    expect(res.body).to.have.property('msg')         
                 })
     })
 })
-    
